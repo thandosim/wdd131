88352a17f55f0fb1676b046b0c28a61104c1b4b9
thandosim <sim24001@byui.edu>
2024-09-21 21:37:27 +0200

    added current year and last modified, working

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;

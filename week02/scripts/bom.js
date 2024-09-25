4f73c9ae1811ac581896f6c6df82c80a1473c0c4
thandosim <sim24001@byui.edu>
2024-09-25 22:40:26 +0200

    finished event listner activity, but deleteButton not working

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,14 +1,25 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+button.addEventListener("click", function() {
+    if (input.value.trim() !== '') {
+        const deleteButton = document.createElement('button');
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+        input.focus(); // to return focus to the inout so that the user can go directly to enter then next chapter.
+    } else {
+        input.focus();        
+    }
+});
 
-li.append(deleteButton);
-// document.querySelector('li').append('deleteButton');
-
-list.append(li);
+deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+});

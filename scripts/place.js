cf67d906163b0adfe54eaa71407cbf58c5b1a6a5
thandosim <sim24001@byui.edu>
2024-10-04 11:26:09 +0200

    fixed function according to rubric

--- scripts/place.js
+++ scripts/place.js
@@ -8,12 +8,15 @@ var chill
 var wind = 10;
 var temperature = 9;
 
-function calculateWindChill(wind,temperature) {
+
     if (temperature <= 10 && wind > 4.8){
-        return Math.round(13.12+(0.6215*temperature)-(11.37*wind**0.16)+(0.3965*temperature*wind**0.16));
+        chill = calculateWindChill(wind,temperature);    
     } else {
-        return "N/A"
+        chill = "N/A";
     }
-}
-chill = calculateWindChill(wind,temperature);
+
 document.getElementById("windChill").textContent =chill;
+
+function calculateWindChill(wind,temperature) {
+    return Math.round(13.12+(0.6215*temperature)-(11.37*wind**0.16)+(0.3965*temperature*wind**0.16));
+}

86a63ad2ec8b6cb6798d638cd37a8e5b686a0558
thandosim <sim24001@byui.edu>
2024-10-22 12:15:30 +0200

    finished nav css for mobile

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -2,3 +2,11 @@ var date = new Date();
 var year = date.getFullYear();
 document.getElementById("currentyear").innerHTML = year;
 document.getElementById("lastModified").textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});

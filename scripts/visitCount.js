8e77e3c72dd3780bcbf32d465073205095dac627
thandosim <sim24001@byui.edu>
2024-10-28 10:11:03 +0200

    fixed isse by changing #visits to .visits

--- scripts/visitCount.js
+++ scripts/visitCount.js
@@ -1,9 +1,9 @@
-const visitsDisplay = document.querySelector("#visits");
+const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 if (numVisits !== 0) {
 	visitsDisplay.innerHTML = numVisits;
 } else {
-	visitsDisplay.innerHTML = `This is the first review.`;
+	visitsDisplay.innerText = `This is the first review.`;
 }
 numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
